8231ee85fefe5673f00844a5ba65e66a445c587f
waynebian01 <150268824+waynebian01@users.noreply.github.com>
2026-04-07 13:25:16 +0800

    Delete Fuyutsui directory

--- Fuyutsui/Fuyutsui/class/hunter_logic.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""猎人职业的基础逻辑（未实现）。"""
-
-
-def run_hunter_logic(state_dict, spec_name):
-    current_step = f"{spec_name or '-'}专精,未实现基础逻辑"
-    return None, current_step, {}
-
